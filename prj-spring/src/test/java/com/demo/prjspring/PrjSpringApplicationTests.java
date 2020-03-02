df78196a7098882a28e6b691f092a2c94f6a5e73
naeline <mouloud.mala@gmail.com>
2020-03-02 12:08:05 +0100

    first commit

---
+++ prj-spring/src/test/java/com/demo/prjspring/PrjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.prjspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
